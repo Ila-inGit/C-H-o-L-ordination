cae31c4df8d3525a01143cb5f9912700cd9a9d10
Ilaria Cappella <ilaria.cappella@weply.com>
2022-10-17 11:47:03 +0200

    refactor: :recycle: Tutorial animation refactor + move old stuff all together

---
+++ Assets/Scripts/TutorialScripts/ActivationTutorial.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit;
+public class ActivationTutorial : MonoBehaviour
+{
+
+    private bool isActive = false;
+
+    public GameObject ContinueButton;
+    public GameObject Tutorial;
+
+    void FixedUpdate()
+    {
+        GetActivationBoxOnLook();
+    }
+
+    public void GetActivationBoxOnLook()
+    {
+        if (!isActive)
+        {
+            if (CoreServices.InputSystem.GazeProvider.GazeTarget)
+            {
+                if (CoreServices.InputSystem.GazeProvider.GazeTarget.tag == "ActivationTutorial")
+                {
+                    isActive = true;
+                    Activate();
+                }
+            }
+
+        }
+    }
+
+
+    public void Activate()
+    {
+        Tutorial.GetComponent<Animator>().SetBool("isActive", true);
+
+        if (ContinueButton != null)
+            ContinueButton.SetActive(true);
+    }
+}
